ad9c686ed1731448cc2ba360efb7ddc0f274498a
Your tafsir <tafsirhossain79@gmail.com>
2021-01-23 22:13:01 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,8 @@
+function kilometerToMeter(kilometer){
+    var Meter = kilometer  * 1000;
+    return Meter;
+}
+var Meter1 = kilometerToMeter(8);
+console.log(Meter1)
+var Meter2 = kilometerToMeter(80);
+console.log(Meter2)
